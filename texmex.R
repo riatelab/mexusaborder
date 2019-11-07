b71e2f874935042ee23c42dec4c3563a252bdf7b
rysebaert <ronan.ysebaert@cnrs.fr>
2019-11-07 18:23:03 +0100

    couche subregions nettoyée + travail sur la prez (premiers commentaires et ordre de la prez

--- texmex.R
+++ texmex.R
@@ -27,26 +27,27 @@ library("animation")
 # 1 - Import des géométries
 ################
 
-# Pays
+# Pays - Natural Earth
 countries <- ne_countries(scale = 50, type = "countries", continent = NULL,
                           country = NULL, geounit = NULL, sovereignty = NULL,
                           returnclass = "sf")
 countries <- countries[countries$adm0_a3 %in% c("MEX","USA"),]
 
-# Rivières
-rivers <- ne_download(scale = 50, type = "rivers_lake_centerlines", category = "physical", returnclass = "sf")
+# Rivières - Natural Earth
+rivers <- ne_download(scale = 50, type = "rivers_lake_centerlines", 
+                      category = "physical", returnclass = "sf")
 
-# Trait de côte
-coastline <- ne_download(scale = 50, type = "coastline", category = "physical", returnclass = "sf")
+# Trait de côte - Natural Earth
+coastline <- ne_download(scale = 50, type = "coastline",
+                         category = "physical", returnclass = "sf")
 
-# Océans 
-ocean <- ne_download(scale = 50, type = "ocean", category = "physical", returnclass = "sf")
+# Océans - Natural Earth
+ocean <- ne_download(scale = 50, type = "ocean", category = "physical",
+                     returnclass = "sf")
 
 # -- Source : Cartographier le monde à l'échelle infranationale (CIST) -- 
-subregions <- st_read(dsn = "data/regions/mex_us_admin_1.shp",options = "ENCODING=UTF-8",
-                      stringsAsFactors = FALSE)
-
-# [TO DO - Régler pb topologiques]
+subregions <- st_read(dsn = "data/regions/mex_us_admin.shp" ,
+                      options = "ENCODING=UTF-8", stringsAsFactors = FALSE)
 
 
 # -- Source : data.world (https://data.world/carlvlewis/border-fence-boundaries-u-s-mexico)
@@ -55,11 +56,13 @@ fences <- geojson_sf("data/data.world/border-fence.geojson")
 
 
 
+
+
 ################
 # 2 - Mise en forme des géométries et création d'un template [Projection Albers]
 ################
 
-# choix de la projection
+# Choix de la projection
 
 albers <- "+proj=aea +lat_1=14.5 +lat_2=32.5 +lat_0=24 +lon_0=-105 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
 countries_aea <- st_transform(countries,crs = albers) 
@@ -67,13 +70,17 @@ fences_aea <- st_transform(fences,crs = albers)
 rivers_aea <- st_transform(rivers,crs = albers) 
 coastline_aea <- st_transform(coastline,crs = albers) 
 ocean_aea <- st_transform(ocean,crs = albers) 
-subregions_aea <- st_transform(subregions,crs = albers) 
+subregions_aea <- st_transform(subregions,crs = albers)
+subregions_all <- st_transform(subregions,crs = albers)
 
-# Choix de l'emprise (un peu de bidouille, pas optimal)
+
+# Choix de l'emprise (étape un peu manuelle pour optimiser l'emprise du modèle carto pour l'export png)
 
 bb_aea <- c(-1342784.0, -739750.5, 793341.2, 1317849.8)
 d <- 100000
-bbox_aea <- st_as_sfc(st_bbox(c(xmin = bb_aea[1]-2*d , xmax = bb_aea[3]+2*d, ymax = bb_aea[2]-d, ymin = bb_aea[4]+d), crs = albers))
+bbox_aea <- st_as_sfc(st_bbox(c(xmin = bb_aea[1]-2*d , xmax = bb_aea[3]+2*d, 
+                                ymin = bb_aea[4]+d, ymax = bb_aea[2]-d), 
+                              crs = albers))
 
 
 # Intersection du fond régional avec la bbox
@@ -83,13 +90,17 @@ coastline_aea <- st_intersection(x = coastline_aea, y = bbox_aea)
 rivers_aea <- st_intersection(x = rivers_aea, y = bbox_aea)
 countries_aea <- st_intersection(x = countries_aea, y = bbox_aea) 
 
+
 # création du template
 
 lay_aea <- function(title = ""){
 par(mar = c(0,0,1.2,0))
-plot(st_geometry(ocean_aea), col= "#b8d5e3", border = NA, xlim = bb_aea[c(1,3)], ylim = bb_aea[c(2,4)])
-plot(st_geometry(subregions_aea) + c(-10000, -10000), col ="#827e6c50", border = NA, add = T)
-plot(st_geometry(subregions_aea), col= "#ede6bb", border = "white", cex = 0.5, add=T)
+plot(st_geometry(ocean_aea), col= "#b8d5e3", border = NA, xlim = bb_aea[c(1,3)],
+     ylim = bb_aea[c(2,4)])
+plot(st_geometry(subregions_aea) + c(-10000, -10000), col ="#827e6c50",
+     border = NA, add = T)
+plot(st_geometry(subregions_aea), col= "#ede6bb", border = "white",
+     cex = 0.5, add=T)
 plot(st_geometry(coastline_aea), col= "#6d9cb3",lwd = 1 ,add= T)
 plot(st_geometry(rivers_aea), col= "#6d9cb3",lwd = 1 ,add= T)
 plot(st_geometry(fences_aea), col= "#3d3c3c",lwd = 3 ,add= T)
@@ -106,11 +117,13 @@ dev.off()
 
 
 
+
+
 ################
 # 3 - Mise en forme des géométries et création d'un template [Projection Orthographique]
 ################
 
-# choix de la projection
+# Choix de la projection
 
 ortho <- "+proj=ortho +lat_0=-35 +lon_0=-104 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs"
 countries_ortho <- st_transform(countries,crs = ortho) 
@@ -120,12 +133,14 @@ coastline_ortho <- st_transform(coastline,crs = ortho)
 ocean_ortho <- st_transform(ocean,crs = ortho) 
 subregions_ortho <- st_transform(subregions,crs = ortho) 
 
-# Choix de l'emprise (un peu de bidouille, pas optimal)
+
+# Choix de l'emprise (étape un peu manuelle pour optimiser l'emprise du modèle carto pour l'export png)
 
 bb_ortho <- c(-1668000, 5100000, 934863, 5900000)
-
 d  <- 100000
-bbox_ortho <- st_as_sfc(st_bbox(c(xmin = bb_ortho[1]-2*d , xmax = bb_ortho[3]+2*d, ymax = bb_ortho[2]-3.5*d, ymin = bb_ortho[4]+3.5*d), crs = ortho))
+bbox_ortho <- st_as_sfc(st_bbox(c(xmin = bb_ortho[1]-2*d , xmax = bb_ortho[3]+2*d, 
+                                  ymin = bb_ortho[4]+3.5*d, ymax = bb_ortho[2]-3.5*d), 
+                                crs = ortho))
 
 # Intersection du fond régional avec la bbox
 
@@ -184,6 +199,9 @@ png("img/fig03.png", width = sizes_ortho[1], height = sizes_ortho[2], res = 150)
 lay_ortho2("Template cartographique 2 (projection orthographique)")
 dev.off()
 
+
+
+
 ################
 # 4 - Import des données et jointures
 ################
@@ -218,15 +236,58 @@ subregions_ortho <- merge (x = subregions_ortho, y = pop,
                          by.y = "ID_ADMIN",
                          all.x = TRUE)
 
+subregions_all <- merge (x = subregions_all, y = pib, 
+                         by.x = "ID_ADMIN_1",
+                         by.y = "ID_ADMIN",
+                         all.x = TRUE)
+
+subregions_all <- merge (x = subregions_all, y = pop, 
+                         by.x = "ID_ADMIN_1",
+                         by.y = "ID_ADMIN",
+                         all.x = TRUE)
+
+
+
+############
+# 5 - Anamorphose population
+#############
+
+# Gestion des multipolygones
+subregions_all <- st_cast(subregions_all, "MULTIPOLYGON")
+
+# Calcul cartogramme
+subregions_anam <- cartogram_cont(subregions_all,weight = "POP_2015", itermax = 30)
+
+png("img/fig04.png", width = sizes_aea[1], height = sizes_aea[2], res = 150)
+
+par(mar = c(0,0,1.2,0))
+
+plot(st_geometry(bbox_aea), col= "#b8d5e3", border = NA, xlim = bb_aea[c(1,3)], ylim = bb_aea[c(2,4)])
+plot(st_geometry(subregions_all), col = "#bcbbbd", border = NA, add = TRUE)
+plot(subregions_anam["ISO3.x"], pal = c("#d1abf7", "#a0b1eb"), border = "white", add = TRUE)
+
+legtxt <- "Sur cette carte,\nla surface\ndes régions\nest proportionnelle\nà la population en 2015."
+text(-1400000, y = 300000, legtxt  , cex = 0.9, pos = 4, font = 2)
+
+layoutLayer(title = "Et si on déformait sur la population ?",
+            author =  authors,
+            scale = FALSE, south = TRUE, frame = TRUE,
+            col = "#6d9cb3", coltitle = "white")
+
+dev.off()
+
+
+
+
+
 ################
-# 5 - Evolution du PIB par habitant dans le temps
+# 7 - Evolution du PIB par habitant dans le temps
 ################
 
 # Télécharger les données de la table PDB_LV pour USA, Mexique, pays de l'OCDE
 
-df <- get_dataset(dataset = "PDB_LV", filter = list(c("MEX", "USA","OECD"), 
-                                                    "T_GDPPOP",
-                                                    "CPC"))
+df <- get_dataset(dataset = "PDB_LV",
+                  filter = list(c("MEX", "USA","OECD"), "T_GDPPOP", "CPC"))
 
 # Transformer la date au format numérique
 
@@ -234,7 +295,7 @@ df$obsTime <- as.numeric(df$obsTime)
 
 # Représentation graphique
 
-png("img/fig04.png", width = 1500, height = 1000, res = 150)
+png("img/fig05.png", width = 1500, height = 1000, res = 150)
 ggplot(data = df, aes(x = obsTime, y = obsValue, color = LOCATION)) + 
   geom_line(size = 1) +  
   labs(x = NULL, y = "Dollars, prix courant", color = NULL,
@@ -244,8 +305,10 @@ ggplot(data = df, aes(x = obsTime, y = obsValue, color = LOCATION)) +
 dev.off()
 
 
+
+
 ################
-# 6 - Evolution des moins de 20 ans dans le temps
+# 8 - Evolution des moins de 20 ans dans le temps
 ################
 
 df <- as.data.frame(get_dataset(dataset = "POP_PROJ", 
@@ -256,23 +319,26 @@ df$obsTime <- as.numeric(df$obsTime)
 
 # Représentation graphique
 
-png("img/fig05.png", width = 1500, height = 1000, res = 150)
+png("img/fig06.png", width = 1500, height = 1000, res = 150)
 ggplot(data = df, aes(x = obsTime, y = obsValue, color = LOCATION)) + 
   geom_line(size = 1) +  
-  labs(x = NULL, y = "Part de la population totale", color = NULL,
-       title =  "Évolution de la part des moins de 20 ans (Mexique - USA - OCDE)") +
+  labs(x = NULL, y = "Part de la population totale (%)", color = NULL,
+       title =  "Évolution comparée des moins de 20 ans (Mexique - USA - OCDE)") +
   theme_hc() +
   scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9"))
 dev.off()
 
 
+
+
+
 ############### 
-# 7 - Cartographie des ruptures spatiales : Vieillissement démographique
+# 9 - Cartographie des ruptures spatiales : Vieillissement démographique
 ###############
 
 # Version 1 ------
 
-png("img/fig06.png", width = sizes_aea[1], height = sizes_aea[2], res = 150)
+png("img/fig07.png", width = sizes_aea[1], height = sizes_aea[2], res = 150)
 
 
 par(mar = c(0,0,1.2,0))
@@ -297,9 +363,7 @@ discLayer(x = subregions.borders, df = subregions_aea,
           legend.pos = c(-1400000 , -300000), legend.title.cex = 0.7, legend.values.cex = 0.5,
           add = TRUE)
 
-
 plot(st_geometry(coastline_aea), col= "#6d9cb3",lwd = 1 ,add= T)
-
 
 layoutLayer(title = "Une barrière démographique...",
             author =  authors,
@@ -308,14 +372,15 @@ layoutLayer(title = "Une barrière démographique...",
 
 dev.off()
 
+
 # Version 2 ------
 
-png("img/fig07.png", width = sizes_ortho[1], height = sizes_ortho[2], res = 150)
+png("img/fig08.png", width = sizes_ortho[1], height = sizes_ortho[2], res = 150)
 
 par(mar = c(0,0,1.2,0))
 plot(st_geometry(bbox_ortho), col= "#b8d5e3", border = NA, xlim = bb_ortho[c(1,3)], ylim = bb_ortho[c(2,4)])
 
-choroLayer(x = subregions_ortho, var = "POP65_POP15",
+choroLayer(x = subregions_ortho, var = "POP65_POP15", 
            breaks = c(min(subregions_ortho$POP65_POP15, na.rm = T),
                       20,25,35,50,65, max(subregions_ortho$POP65_POP15, na.rm = T)),
            col = carto.pal(pal1 = "green.pal", n1 = 3, pal2 = "red.pal", n2 = 3),
@@ -344,6 +409,7 @@ layoutLayer(title = "Une barrière démographique...",
 
 dev.off()
 
+
 # Version 3 ------
 
 threshold <- 0.3
@@ -351,20 +417,21 @@ minvar <- as.numeric(quantile(discontinuities$disc, probs = c(1 - threshold)))
 discontinuities <- discontinuities[discontinuities$disc >= minvar,]
 discontinuities$height <- round(discontinuities$disc / 2,0)
 
+
 extrude <- function(id){
   line <- st_geometry(discontinuities[id,])
-  plot(line, col= "black",lwd = 2 ,add= T)
+  plot(line, col = "black",lwd = 2 ,add = TRUE)
   nb <- as.numeric(discontinuities[id,"height"])[1]
   for (j in 1:nb){
     line <- st_geometry(line) + c(0,5000)
-    plot(st_geometry(line), col= "#ebd23490",lwd = 2 ,add= T)  
+    plot(st_geometry(line), col= "#ebd23490",lwd = 2 ,add = TRUE)  
   }
-  plot(line, col= "black",lwd = 2 ,add= T)
+  plot(line, col= "black",lwd = 2 ,add = TRUE)
 }
 
 
 
-png("img/fig08.png", width = sizes_ortho[1], height = sizes_ortho[2], res = 150)
+png("img/fig09.png", width = sizes_ortho[1], height = sizes_ortho[2], res = 150)
 
 par(mar = c(0,0,1.2,0))
 plot(st_geometry(bbox_ortho), col= "#b8d5e3", border = NA, xlim = bb_ortho[c(1,3)], ylim = bb_ortho[c(2,4)])
@@ -393,13 +460,16 @@ layoutLayer(title = "Une barrière démographique...",
 
 dev.off()
 
+
+
+
 ############### 
-# 8 -  Cartographie des ruptures spatiales : PIB par habitant
+# 10 -  Cartographie des ruptures spatiales : PIB par habitant
 ###############
 
 # Version 1 ------
 
-png("img/fig09.png", width = sizes_aea[1], height = sizes_aea[2], res = 150)
+png("img/fig10.png", width = sizes_aea[1], height = sizes_aea[2], res = 150)
 
 par(mar = c(0,0,1.2,0))
 plot(st_geometry(bbox_aea), col= "#b8d5e3", border = NA, xlim = bb_aea[c(1,3)], ylim = bb_aea[c(2,4)])
@@ -436,7 +506,7 @@ dev.off()
 
 # Version 2 ------
 
-png("img/fig10.png", width = sizes_ortho[1], height = sizes_ortho[2], res = 150)
+png("img/fig11.png", width = sizes_ortho[1], height = sizes_ortho[2], res = 150)
 
 par(mar = c(0,0,1.2,0))
 plot(st_geometry(bbox_ortho), col= "#b8d5e3", border = NA, xlim = bb_ortho[c(1,3)], ylim = bb_ortho[c(2,4)])
@@ -490,7 +560,20 @@ extrude <- function(id){
 
 
 
-png("img/fig11.png", width = sizes_ortho[1], height = sizes_ortho[2], res = 150)
+extrude <- function(id){
+  line <- st_geometry(discontinuities[id,])
+  plot(line, col= "black",lwd = 2 ,add= T)
+  nb <- as.numeric(discontinuities[id,"height"])[1]
+  for (j in 1:nb){
+    line <- st_geometry(line) + c(0,5000)
+    plot(st_geometry(line), col= "#ebd23490",lwd = 2 ,add= T)  
+  }
+  plot(line, col= "black",lwd = 2 ,add= T)
+}
+
+
+
+png("img/fig12.png", width = sizes_ortho[1], height = sizes_ortho[2], res = 150)
 
 par(mar = c(0,0,1.2,0))
 plot(st_geometry(bbox_ortho), col= "#b8d5e3", border = NA, xlim = bb_ortho[c(1,3)], ylim = bb_ortho[c(2,4)])
@@ -514,66 +597,19 @@ for (i in 1:length(discontinuities$disc))
 }
 legtxt <- "Sur cette carte, la hauteur\ndes barrières est proportionnelle\nà la valeur des discontinuités\nabsolues sur le PIB par habitant\nen 2017."
 text(-1700000, y = 5400000, legtxt  , cex = 0.9, pos = 4, font = 2) 
-layoutLayer(title = "Une barrière démographique...",
-            author =  authors,
-            scale = 300, south = TRUE, frame = TRUE,
-            col = "#6d9cb3", coltitle = "white")
-
-
-
-
-dev.off()
-
-############
-# 9 - Anamorphose population / richesse
-#############
-
-# Gestion des multipolygones
-subregions_aea <- st_cast(subregions_aea, "MULTIPOLYGON")
-
-# Calcul cartogramme
-subregions_anam <- cartogram_cont(subregions_aea,weight = "POP_2015", itermax = 30)
-
-png("img/fig12.png", width = sizes_aea[1], height = sizes_aea[2], res = 150)
-
-par(mar = c(0,0,1.2,0))
-
-plot(st_geometry(bbox_aea), col= "#b8d5e3", border = NA, xlim = bb_aea[c(1,3)], ylim = bb_aea[c(2,4)])
-
-
-# Cartographie
-choroLayer(x = subregions_anam, var = "PIB100_2017",
-           breaks = c(min(subregions_anam$PIB100_2017, na.rm = T),
-                      75,90,100,125,150,200, max(subregions_anam$PIB100_2017, na.rm = T)),
-           col = carto.pal(pal1 = "red.pal", n1 = 3, pal2 = "green.pal", n2 = 5),
-           legend.pos = c(-1400000 , -600000),
-           legend.horiz = TRUE, legend.title.cex = 0.7, legend.values.cex = 0.5,
-           legend.title.txt = "PIB par habitant 2017\n(100 = moyenne mondiale)",
-           border = "white", add=T)
-
-# Et discontinuités associées
-subregions_anamBor <- getBorders(subregions_anam)
-
-discLayer(x = subregions_anamBor, df = subregions_aea,
-          var = "PIB100_2017", col="black", nclass=3,
-          method="equal", threshold = 0.2, sizemin = 0.5,
-          sizemax = 10, type = "abs",legend.values.rnd = 0,
-          legend.title.txt = "Discontinuités de PIB par habitant 2017\n(différences absolues)",
-          legend.pos = c(-1400000 , -300000), legend.title.cex = 0.7, legend.values.cex = 0.5,
-          add = TRUE)
-
-legtxt <- "Sur cette carte,\nla surface\ndes régions\nest proportionnelle\nau PIB en 2017."
-text(-1400000, y = 300000, legtxt  , cex = 0.9, pos = 4, font = 2)
-
-layoutLayer(title = "Et si on déformait sur la population ?",
+layoutLayer(title = "Doublée d'un mur de richesse... Mais quelles conséquences ?",
             author =  authors,
             scale = 300, south = TRUE, frame = TRUE,
             col = "#6d9cb3", coltitle = "white")
 
 dev.off()
 
+
+
+
+
 ############
-# 10 - Border Control - Visualisation des postes de contrôle
+# 11 - Border Control - Visualisation des postes de contrôle
 #############
 
 # Convertir la bounding box en WGS 84
@@ -617,7 +653,6 @@ grid <- st_sf(grid)
 
 # Compter le nombre de postes de police par points de grille
 grid$ncops <- lengths(st_covers(grid, poi_osm))
-#grid$dcops <- grid$ncops / set_units(st_area(grid), "km^2")
 
 # Plusieurs façons de visualiser les résultats (tests)
 png("img/fig14.png", width = sizes_aea[1], height = sizes_aea[2], res = 150)
